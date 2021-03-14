10418252da1b61d3681775a401a1066bba9b7ad9
john <john.d.leonetti@gmail.com>
2021-03-14 13:56:48 -0700

    commit tree 482b0dc8d4e40f982afce81466f999d558194232

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,36 @@
 # Write your code here
+
+require 'open-uri'
+require 'net/http'
+require "json"
+require 'pry'
+
+class GetRequester
+
+    def initialize(url)
+        @URL = "http://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    end
+
+    def get_response_body
+       
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+       
+    end
+
+    def parse_json
+            # response_body = JSON.parse(self.get_response_body)
+            # response_body.collect do |respond|
+            JSON.parse(get_response_body)
+
+
+    end
+
+
+
+
+end
+
+# response = GetRequester.new.get_response_body
+# puts response
